7206ce0e9f8c64cccc6b5ef53a504b4b44b08a0f
周先森 <wemadecode@gmail.com>
2019-04-24 17:00:33 +0800

    bug fix

--- ZXPageView/Classes/ZXWaterViewLayout.swift
+++ ZXPageView/Classes/ZXWaterViewLayout.swift
@@ -19,12 +19,9 @@ public class ZXWaterViewLayout: UICollectionViewFlowLayout {
     /// 列数
     public var cols = 2
     private lazy var attrsArray  = [UICollectionViewLayoutAttributes]()
-    private var maxH : CGFloat = 0
-    private var startIndex = 0
-    private lazy var colHeights : [CGFloat] = {
-        var colHeights = Array(repeating: self.sectionInset.top, count: self.cols)
-        return colHeights
-    }()
+    private var maxHeight : CGFloat = 0
+    // 所有列的总高度
+    private lazy var colHeights = [CGFloat]()
 }
 
 
@@ -32,65 +29,76 @@ extension ZXWaterViewLayout {
     //MARK: - 初始化生成每个视图的布局信息
     override public func prepare() {
         super.prepare()
-        
-        // 1.对collectionView进行判空校验
+
         guard let collectionView = collectionView else { return }
         
-        // 2.获取item的个数(仅考虑一个section的情况)
-        let itemCount = collectionView.numberOfItems(inSection: 0)
+        //高度高度数组
+        colHeights = Array(repeating: self.sectionInset.top, count: self.cols)
         
-        // 3.计算item的宽度
-        let itemW = (collectionView.bounds.width - self.sectionInset.left - self.sectionInset.right - self.minimumInteritemSpacing * CGFloat((cols - 1))) / CGFloat(cols)
+        //清除布局信息
+        attrsArray.removeAll()
         
-        // 3.计算所有的item的属性
-        for itemIndex in startIndex..<itemCount {
-            // 3.1设置每一个item位置相关的属性
-            let indexPath = IndexPath(item: itemIndex, section: 0)
+        //获取行数(仅考虑一个section的情况)
+        let rowCount = collectionView.numberOfItems(inSection: 0)
+       
+        for row in 0..<rowCount{
+            //创建位置
+            let indexPath = IndexPath(item: row, section: 0)
             
-            // 3.2根据位置创建Attributes属性
-            let attrs = UICollectionViewLayoutAttributes(forCellWith: indexPath)
-            
-            // 3.3获取item的高度
-            guard let itemH = dataSource?.waterView(self, heightForRowAt: indexPath) else{
-                fatalError("请设置数据源,并且实现对应的数据源方法")
+            //计算indexPath位置cell对应的布局属性
+            if let attrs = self.layoutAttributesForItem(at: indexPath){
+                attrsArray.append(attrs)
             }
-         
-            // 3.4取出最小列的位置
-            var minH = colHeights.min()!
-            let index = colHeights.index(of: minH)!
-            minH = minH + itemH + minimumLineSpacing
-            colHeights[index] = minH
-            
-            // 3.5设置item的属性
-            let x = self.sectionInset.left + (self.minimumInteritemSpacing + itemW) * CGFloat(index)
-            let y =  minH - itemH - self.minimumLineSpacing
-            attrs.frame = CGRect(x: x , y: y, width: itemW, height: itemH)
-            
-            // 3.6添加到数组中
-            attrsArray.append(attrs)
         }
-        
-        // 4.记录最大值
-        maxH = colHeights.max()!
-        
-        // 5.给startIndex重新复制
-        startIndex = itemCount
     }
-}
-
-
-// MARK:- 返回决定一段区域所有cell和头尾视图的布局属性
-extension ZXWaterViewLayout {
+    
+    // MARK:- 返回决定一段区域所有cell和头尾视图的布局属性
     override public func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         return attrsArray
     }
-}
-
-
-
-// MARK:- 设置可滚动的区域
-extension ZXWaterViewLayout {
+    
+    // MARK:- 设置可滚动的区域
     override public var collectionViewContentSize: CGSize {
-        return CGSize(width: 0, height: maxH + sectionInset.bottom - minimumLineSpacing)
+        return CGSize(width: 0, height: maxHeight + sectionInset.bottom - minimumLineSpacing)
     }
+    
+    // MARK:- 返回indexPath位置cell对应的布局属性
+    override public func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let collectionView = collectionView else { return nil }
+        
+        //创建布局属性
+        let attrs = UICollectionViewLayoutAttributes(forCellWith: indexPath)
+ 
+        //计算宽度
+        let itemW = (collectionView.bounds.width - self.sectionInset.left - self.sectionInset.right - self.minimumInteritemSpacing * CGFloat((cols - 1))) / CGFloat(cols)
+        
+        //获取高度
+        guard let itemH = dataSource?.waterView(self, heightForRowAt: indexPath) else{
+            fatalError("请设置数据源,并且实现对应的数据源方法")
+        }
+        
+        // 取出最小列的值
+        guard var minH = colHeights.min() else { return nil }
+        guard let index = colHeights.index(of: minH) else { return nil}
+        minH = minH + itemH + minimumLineSpacing
+        // 更新最短那列的高度
+        colHeights[index] = minH
+        
+        // 记录最大高度
+        if maxHeight < minH{
+            maxHeight = minH
+        }
+        
+        //计算横坐标
+        let itemX = self.sectionInset.left + (self.minimumInteritemSpacing + itemW) * CGFloat(index)
+        //计算纵坐标
+        let itemY =  minH - itemH - self.minimumLineSpacing
+        
+        attrs.frame = CGRect(x: itemX , y: itemY, width: itemW, height: itemH)
+
+        return attrs
+    }
+    
 }
+
+
